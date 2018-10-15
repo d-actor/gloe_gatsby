2311e5340ccbc6cbb471455bab2189e616fd698b
Dan Actor <danltactor@gmail.com>
2018-10-15 15:07:23 -0600

    update temporary index to deploy

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import { Link } from 'gatsby';
+import Gloego from '../images/gloelogo.png';
+
+const styles = {
+  image: {
+    height: '15vw',
+  },
+}
 
 const Header = ({ siteTitle }) => (
   <div
     style={{
       margin: '0 auto',
       maxWidth: 960,
-      padding: '1.45rem 1.0875rem',
+      padding: '10vw 1.0875rem 1.45rem 1.0875rem',
+      textAlign: 'center',
     }}
   >
     <h1 style={{ margin: 0 }}>
@@ -17,7 +25,7 @@ const Header = ({ siteTitle }) => (
           textDecoration: 'none',
         }}
       >
-        {siteTitle}
+        <img src={Gloego} alt={siteTitle} style={styles.image} />
       </Link>
     </h1>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import Layout from '../components/layout'
 const styles = {
   text: {
     color: '#FFF',
-  }
+    textAlign: 'center',
+    fontWeight: '100',
+  },
 }
 
 const IndexPage = () => (
   <Layout>
-    <h1 style={styles.text}>Stay Tuned...</h1>
+    <h1 style={styles.text}>...Stay Tuned...</h1>
   </Layout>
 )
 
