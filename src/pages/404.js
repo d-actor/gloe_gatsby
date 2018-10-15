373731630fffccb80845b127f78b2cf2b4ab7218
Dan Actor <danltactor@gmail.com>
2018-10-15 11:12:40 -0600

    404 update

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,10 +2,21 @@ import React from 'react'
 import Layout from '../components/layout'
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... sit on that for a minute.</p>
-  </Layout>
+  <div style={styles.main}>
+    <Layout>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... sit on that for a minute.</p>
+    </Layout>
+  </div>
 )
 
+const styles = {
+  main: {
+    color: '#fff',
+    textAlign: 'center',
+    marginTop: '5vw',
+  },
+}
+
 export default NotFoundPage
+
